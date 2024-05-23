8f931bac2cb81e9836487cdfdc99ec737870e633
KingGibson <gibson.kingori@strathmore.edu>
2024-05-23 12:36:14 +0300

    php

--- in.php
+++ in.php
@@ -6,8 +6,8 @@ echo "This is my first php"; //Displaying a sentence or a string
 
 print "<br>";//Using HTML's <br> to break a line
 
-print "Today < span style ='color: #ff4856;
-text-transform;uppercase;'> Thursday</span";
+print "Today <span style ='color: #ff4856;
+text-transform;uppercase;'> Thursday";
 
 print "<br>"; //Using HTML's <br> to break a line
 
@@ -19,16 +19,69 @@ print "5475";//THIS IS  A STRING
 
 print "<br>";//Using HTML's <br> to break a line
 
-print date('1'); //Displaying the current day of the week
+print date('l'); //Displaying the current day of the week
 
 print "<br>"; //Using HTML's <br> to break a line
 
-print date('L, F jS Y', strtotime('+5 months'));
+print date('l, F jS Y', strtotime('+5 months'));
 
 print "<br>";//Using HTML's <br> to break a line
 
 print "Today is " .date('l, F jS Y H:i:s');
 
+
+//Creating (Declaring) a varriable
+
+$fname = "Alex Okama"; //Declaration of a variable or a string or group of words
+
+$yob = 1999; //Declaration of digit or an integer
+
+print "<br>";
+
+print $fname;
+
+print "<br>"; //Using HTML's <br> to break a line
+
+echo $yob;
+
+print "<br>"; //Using HTML's <br> to break a line
+
+$current_year = date('Y');
+
+echo $current_year;
+
+print "<br>"; //Using HTML's <br> to break a line
+
+$age = $current_year - $yob;
+
+print $fname . "  is actually " . $interval->y ."years" .$interval->m . "months, and ";
+
+print $age;
+
+$user_dob ="1999-05-26";
+
+echo $fname . "was born in " .$yob;
+
+print "<br>"; //Using HTML's <br> to break a line
+
+echo $fname . "was actually born on " .$user_dob;
+
+print "45+96";
+
+print "<br>"; //Using HTML's <br> to break a line
+
+print 45+69;
+
+
+$birthday = new DateTime($user_dob);
+$today = new DateTime('Today');
+
+$interval = $birthday->diff($today);
+
+
+echo '<pre>';
+print_r($interval);
+echo '</pre>';
 
 
 
