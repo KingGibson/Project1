23df20783cc2c92bc43803ed98b46a17ac67dd15
KingGibson <gibson.kingori@strathmore.edu>
2024-05-23 11:54:32 +0300

    php coding

---
+++ in.php
@@ -0,0 +1,32 @@
+<?php 
+
+echo "This is my first php"; //Displaying a sentence or a string
+
+print "<br>";//Using HTML's <br> to break a line
+
+print "Today < span style ='color: #ff4856;
+text-transform;uppercase;'> Thursday</span";
+
+print "<br>"; //Using HTML's <br> to break a line
+
+print 1999;
+
+print "<br>";//Using HTML's <br> to break a line
+
+print "5475";//THIS IS  A STRING
+
+print "<br>";//Using HTML's <br> to break a line
+
+print date('1'); //Displaying the current day of the week
+
+print "<br>"; //Using HTML's <br> to break a line
+
+print date('d/m/y');
+
+
+
+
+
+
+
+?>
