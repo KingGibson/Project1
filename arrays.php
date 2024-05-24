a1ec0c4ee1119ba030afdd442f938425854eaa44
KingGibson <gibson.kingori@strathmore.edu>
2024-05-24 10:56:40 +0300

    array

--- arrays.php
+++ arrays.php
@@ -1,5 +1,36 @@
 <?php 
+//indexed or Numeric Array
+$colors = [ "Black", "Green","Yellow"];
 
-
+print_r ($colors);
 
 ?>
+
+<br>
+
+<?php
+ 
+//Indexed Array
+$user = array ("Alex", "Peter", "Ann");
+print $user[2];
+?>
+
+<pre>
+    <?php print_r ($user);?>
+</pre>
+
+<?php 
+//Associated Array
+
+$user_data= [
+    "Fullname"=> "Alex Okama",
+    "Email"=> "AOkama@yahoo.com",
+    "Phone"=> "+25434567898"
+];
+
+print $user_data["Email"];
+?>
+<pre>
+    <?php print_r($user_data);?>
+</pre>
+
