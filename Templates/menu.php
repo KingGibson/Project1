ab4c6e9282005d6ceaddbde9d82038a2dc9679bb
KingGibson <gibson.kingori@strathmore.edu>
2024-06-19 18:38:58 +0300

    php file

---
+++ Templates/menu.php
@@ -0,0 +1,186 @@
+<!--Our menu section start-->
+<section class="our-menu" id="menu">
+  <h1 class="heading">our food menu</h1>
+  <center><h3 class="sub-heading"> ~ see what we offer ~ </h3></center>
+
+  <div class="menu-container">
+
+    <div class="item">
+      <div class="item-name">
+        <h2>Main Course</h2>
+        <img src="https://localhost/project1/img/drinks.png" alt="">
+         </div><!--item-name div ends-->
+
+
+
+         <div class="item-body">
+            <div class="item-menu">
+              <h3>Super-Delicious Zuppa Toscana</h3>
+              <span class="dots"></span>
+              <h3>$40</h3>
+
+              <ul>
+                <li><a href="#">Chicken</a></li>
+                <li><a href="#">Italian</a></li>
+                <li><a href="#">Sausages</a></li>
+                <li><a href="#">Spinach</a></li>
+              </ul>
+           </div><!--item-menu div ends-->
+
+
+           <div class="item-menu">
+            <h3>Super-Delicious Zuppa Toscana</h3>
+            <span class="dots"></span>
+            <h3>$26</h3>
+
+            <ul>
+              <li><a href="#">Mushrooms</a></li>
+              <li><a href="#">Italian</a></li>
+              <li><a href="#">Sausages</a></li>
+              <li><a href="#">Spinach</a></li>
+            </ul>
+         </div><!--item-menu div ends-->
+
+
+             </div><!--item-body div ends-->
+                  </div><!--Items div ends-->
+
+
+                  <div class="item">
+                    <div class="item-name">
+                      <h2>Soups & salads</h2>
+                      <img src="https://localhost/project1/img/drinks.png" alt="">
+                       </div><!--item-name div ends-->
+              
+              
+              
+                       <div class="item-body">
+                          <div class="item-menu">
+                            <h3>Super-Delicious Zuppa Toscana</h3>
+                            <span class="dots"></span>
+                            <h3>$40</h3>
+              
+                            <ul>
+                              <li><a href="#">Chicken</a></li>
+                              <li><a href="#">Italian</a></li>
+                              <li><a href="#">Sausages</a></li>
+                              <li><a href="#">Spinach</a></li>
+                            </ul>
+                         </div><!--item-menu div ends-->
+              
+              
+                         <div class="item-menu">
+                          <h3>Super-Delicious Zuppa Toscana</h3>
+                          <span class="dots"></span>
+                          <h3>$26</h3>
+              
+                          <ul>
+                            <li><a href="#">Mushrooms</a></li>
+                            <li><a href="#">Italian</a></li>
+                            <li><a href="#">Sausages</a></li>
+                            <li><a href="#">Spinach</a></li>
+                          </ul>
+                       </div><!--item-menu div ends-->
+              
+              
+                            </div><!--item-body div ends-->
+                                </div><!--Items div ends-->
+              
+
+
+
+                                    <div class="item">
+                                      <div class="item-name">
+                                        <h2>Drinks</h2>
+                                    <img src="https://localhost/project1/img/drinks.png" alt="">
+                                     </div><!--item-name div ends-->
+                            
+                            
+                            
+                                     <div class="item-body">
+                                        <div class="item-menu">
+                                          <h3>Super-Delicious Zuppa Toscana</h3>
+                                          <span class="dots"></span>
+                                          <h3>$40</h3>
+                            
+                                          <ul>
+                                            <li><a href="#">Chicken</a></li>
+                                            <li><a href="#">Italian</a></li>
+                                            <li><a href="#">Sausages</a></li>
+                                            <li><a href="#">Spinach</a></li>
+                                          </ul>
+                                       </div><!--item-menu div ends-->
+                            
+                            
+                                       <div class="item-menu">
+                                        <h3>Super-Delicious Zuppa Toscana</h3>
+                                        <span class="dots"></span>
+                                        <h3>$26</h3>
+                            
+                                        <ul>
+                                          <li><a href="#">Mushrooms</a></li>
+                                          <li><a href="#">Italian</a></li>
+                                          <li><a href="#">Sausages</a></li>
+                                          <li><a href="#">Spinach</a></li>
+                                        </ul>
+                                     </div><!--item-menu div ends-->
+                            
+                            
+                                          </div><!--item-body div ends-->
+                                              </div><!--Items div ends-->
+                            
+
+
+
+                                              <div class="item">
+                                                <div class="item-name">
+                                                  <h2>Desserts</h2>
+                                                  <img src="https://localhost/project1/img/drinks.png" alt="">
+                                                   </div><!--item-name div ends-->
+                                          
+                                          
+                                          
+                                                   <div class="item-body">
+                                                      <div class="item-menu">
+                                                        <h3>Super-Delicious Zuppa Toscana</h3>
+                                                        <span class="dots"></span>
+                                                        <h3>$40</h3>
+                                          
+                                                        <ul>
+                                                          <li><a href="#">Chicken</a></li>
+                                                          <li><a href="#">Italian</a></li>
+                                                          <li><a href="#">Sausages</a></li>
+                                                          <li><a href="#">Spinach</a></li>
+                                                        </ul>
+                                                     </div><!--item-menu div ends-->
+                                          
+                                          
+                                                     <div class="item-menu">
+                                                      <h3>Super-Delicious Zuppa Toscana</h3>
+                                                      <span class="dots"></span>
+                                                      <h3>$26</h3>
+                                          
+                                                      <ul>
+                                                        <li><a href="#">Mushrooms</a></li>
+                                                        <li><a href="#">Italian</a></li>
+                                                        <li><a href="#">Sausages</a></li>
+                                                        <li><a href="#">Spinach</a></li>
+                                                      </ul>
+                                                   </div><!--item-menu div ends-->
+                                          
+                                          
+                                                        </div><!--item-body div ends-->
+                                                            </div><!--Items div ends-->
+                                          
+
+                 
+
+
+
+
+
+
+
+  </div><!--Menu container div ends-->
+</section>
+<!--Our menu section ends-->
---
+++ Templates/welcome.php
@@ -0,0 +1,55 @@
+<!--Welcome section Start-->
+<section class="welcome" id="About">
+  <h1 class="heading">WELCOME TO ROYATE</h1>
+  <center><h3 class="sub-heading"> ~ Luxury & Quality ~ </h3></center>
+
+  <div class="box-container">
+    <div class="box">
+      <div class="image">
+        <img src="https://localhost/project1/img/post-thumb-1.jpg" alt="">
+      </div>
+
+      <div class="content">
+        <h3>PROFESSIONAL LEVEL</h3>
+        <P>nor again is there anyone who loes or pursues or desires to
+          obtain pain of itselt, because it is pain, but because occasionally
+          circumstances occur.</P>
+
+          <a href="#" class="btn">Read More</a>
+      </div>
+    </div>
+
+    <div class="box">
+      <div class="image">
+        <img src="https://localhost/project1/img/post-thumb-2.jpg" alt="">
+      </div>
+
+      <div class="content">
+        <h3>FRESH FOOD GUARANTEED</h3>
+        <P>nor again is there anyone who loes or pursues or desires to
+          obtain pain of itselt, because it is pain, but because occasionally
+          circumstances occur.</P>
+
+          <a href="#" class="btn">Read More</a>
+      </div>
+    </div>
+
+    <div class="box">
+      <div class="image">
+        <img src="https://localhost/project1/img/post-thumb-3.jpg" alt="">
+      </div>
+
+      <div class="content">
+        <h3>THE MENU IS PLENTIFUL</h3>
+        <P>nor again is there anyone who loes or pursues or desires to
+          obtain pain of itselt, because it is pain, but because occasionally
+          circumstances occur.</P>
+
+          <a href="#" class="btn">Read More</a>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<!--Welcome section End-->
