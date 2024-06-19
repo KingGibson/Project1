842cfbc6ed6b3e6063c67dd5704826e422b80704
KingGibson <gibson.kingori@strathmore.edu>
2024-06-19 13:15:00 +0300

    include php

--- index.php
+++ index.php
@@ -14,29 +14,7 @@
 
 </head>
 <body>
-  <!--Header Section Start-->
-  <header>
-    <a href="#" class="logo"><img src="./img/logo.png" alt=""></a>
-
-    <nav class="navbar">
-    <a href="#Home"class="active">Home</a>
-    <a href="#About">About</a>
-    <a href="#Menu">Menu</a>
-    <a href="#Team">Team</a>
-    <a href="#Reservation">Reservation</a>
-    <a href="#Blog">Blog</a>
-
-    </nav>
-
-    <div class="icons">
-      <i class="fas fa-bars" id="Menu"></i>
-      <i class="fas fa-search"></i>
-      <i class="fas fa-heart"></i>
-      <i class="fas fa-shopping-cart"></i>
-
-    </div>
-  </header>
-  <!--Header Section End-->
+  <? include_once "Nav.php"; ?> <!-- NavBar -->
 
   <!--slider section start-->
   <div class="home" id="home">
