8d3658c1c9e742003928710bfb10b47f04d1b8e7
KingGibson <gibson.kingori@strathmore.edu>
2024-06-20 12:12:18 +0300

    news

--- index.php
+++ index.php
@@ -73,8 +73,62 @@
 
   <?php include_once ("templates/Reservation.php"); ?> <!-- Team php -->
 
+<!--News section Start-->
+<section class="blog" id="blog">
+  <h1 class="heading">latest news</h1>
+  <center><h3 class="sub-heading"> ~Greate Artilces</h3></center>
 
-  
+  <div class="box-container">
+    <div class="box">
+      <div class="image">
+        <img src="https://localhost/project1/img/post-thumb-4.jpg" alt="">
+      </div>
+
+      <div class="content">
+        <h3>PROFESSIONAL LEVEL</h3>
+        <P>nor again is there anyone who loves or pursues or desires to
+          obtain pain of itself,because it is pain, but because occasionally
+        circumstances occur</P>
+        <a href="#">READ MORE</a>
+        <img src="https://localhost/project1/img/post-body-bg-1.png" alt="">
+      </div>
+    </div>
+
+
+    <div class="box">
+      <div class="image">
+        <img src="https://localhost/project1/img/post-thumb-5.jpg" alt="">
+      </div>
+
+      <div class="content">
+        <h3>FRESH FOOD GUARENTEED</h3>
+        <P>nor again is there anyone who loves or pursues or desires to
+          obtain pain of itself,because it is pain, but because occasionally
+        circumstances occur</P>
+        <a href="#">READ MORE</a>
+        <img src="https://localhost/project1/img/post-body-bg-2.png" alt="">
+      </div>
+    </div>
+
+
+    <div class="box">
+      <div class="image">
+        <img src="https://localhost/project1/img/post-thumb-6.jpg" alt="">
+      </div>
+
+      <div class="content">
+        <h3>THE MENU PLENTIFUL</h3>
+        <P>nor again is there anyone who loves or pursues or desires to
+          obtain pain of itself,because it is pain, but because occasionally
+        circumstances occur</P>
+        <a href="#">READ MORE</a>
+        <img src="https://localhost/project1/img/post-body-bg-3.png" alt="">
+      </div>
+    </div>
+  </div>
+
+</section>
+ <!--News section End-->
 
 
 
