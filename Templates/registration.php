85128a35ab9ba67d154d46ed9c7ba91d9e596c41
KingGibson <gibson.kingori@strathmore.edu>
2024-06-21 11:08:16 +0300

    register

--- Templates/registration.php
+++ Templates/registration.php
@@ -6,6 +6,23 @@
     <title>Document</title>
 </head>
 <body>
+<div class="header">
+        <div class="nav"></div>
+    </div>
     
+    <div class="container">
+        <form action="" method="post">
+            <h1>Welcome</h1>
+            <input name="FirstName" type="text" placeholder="FirstName">
+            <br>
+            <input name="LastName" type="text" placeholder="LastName">
+            <br>
+            <input name="email" type="text" placeholder="Email">
+            <br>
+            <input name="password" type="text" placeholder="password">
+            <br>
+            <button name="submit">Register</button>
+        </form>
+    </div>
 </body>
 </html>
